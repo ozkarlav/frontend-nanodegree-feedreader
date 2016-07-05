eb08e9710e25b67a25c080ec36d96ca84a313e8d
ozkarlav <ozkarlav@yahoo.com>
2016-07-05 17:13:26 -0400

    First commit Added first test that loops through each feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        function testUrl(feed){
+            it('urls are defined', function(){
+               expect(feed).toBeDefined();
+               expect(feed).not.toBe('');
+            });
+        };
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
